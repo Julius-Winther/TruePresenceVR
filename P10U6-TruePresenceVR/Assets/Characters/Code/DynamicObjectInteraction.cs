19d690a47f83513d05d69f619d4acd1737052fe5
LukasLN <72730717+LukasLN@users.noreply.github.com>
2025-04-21 14:31:39 +0200

    Small test of dynamic object interaction added

---
+++ P10U6-TruePresenceVR/Assets/Characters/Code/DynamicObjectInteraction.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Convai.Scripts.Runtime.Features;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using System;
+
+public class DynamicObjectInteraction : MonoBehaviour
+{
+    private DynamicInfoController _dynamicInfoController;
+    [SerializeField] private string objectName;
+    private void Awake()
+    {
+        _dynamicInfoController = GetComponent<DynamicInfoController>();
+    }
+    public void OnGrab()
+    {
+        _dynamicInfoController.SetDynamicInfo("User/Player is currently holding " + objectName);
+    }
+    public void OnDrop()
+    {
+        _dynamicInfoController.SetDynamicInfo("User/Player droppped " + objectName + " and is no longer holding anything");
+    }
+}
